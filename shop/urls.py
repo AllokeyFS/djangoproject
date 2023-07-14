2e6a4deae49cc4b9e836ed33c2fcb7ffcc456c90
AllokeyFS <atosh.rami@gmail.com>
2023-07-14 20:34:40 +0600

    added photos

--- shop/urls.py
+++ shop/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import author_list, book_list, category_list, movie_list_90, user_list, new_movies_list
+from .views import author_list, book_list, category_list, movie_list_90, user_list, new_movies_list, author_list_alive, book_list_90, book_list_00, movie_list_budget_lt_50
 
 
 urlpatterns = [
@@ -9,5 +9,8 @@ urlpatterns = [
     path('movie/', movie_list_90, name='movie_list'),
     path('user/', user_list, name='user_list'),
     path('new_movie/', new_movies_list, name='movie_list'),
-
+    path('author_alive/', author_list_alive, name='author_list_alive'),
+    path('book_90/', book_list_90, name='book_list_90'),
+    path('book_00/', book_list_00, name='book_list_00'),
+    path('movie_budget_lt_50/', movie_list_budget_lt_50, name='movie_list_budget_lt_50'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -6,8 +6,20 @@ def author_list(request):
     authors = Author.objects.all()
     return render(request, 'html/index.html', {'authors': authors})
 
+def author_list_alive(request):
+    authors = Author.objects.filter(date_of_death = None)
+    return render(request, 'html/index.html', {'authors': authors})
+
 def book_list(request):
-    books = Book.objects.all()
+    books = Book.objects.filter(in_stock = True)
+    return render(request, 'html/book.html', {'books': books})
+
+def book_list_90(request):
+    books = Book.objects.filter(publication_date__lt = date(2000,1,1), publication_date__gte = date(1990,1,1))
+    return render(request, 'html/book.html', {'books': books})
+
+def book_list_00(request):
+    books = Book.objects.filter(publication_date__lt = date(2011,1,1), publication_date__gte = date(2000,1,1))
     return render(request, 'html/book.html', {'books': books})
 
 def category_list(request):
@@ -18,10 +30,14 @@ def movie_list_90(request):
     movies = Movie.objects.filter(year_release__lt = date(2000, 1, 1), year_release__gt = date(1990,1,1))
     return render(request, 'html/movie.html', {'movies': movies})
 
+def movie_list_budget_lt_50(request):
+    movies = Movie.objects.filter(budget__lte = 50000000)
+    return render(request, 'html/movie.html', {'movies': movies})
+
 def user_list(request):
     users = Users.objects.all()
     return render(request, 'html/user.html', {'users': users})
 
 def new_movies_list(request):
     movies = Movie.objects.filter(year_release__gte = date(2020,1,1))
-    return render(request, 'html/new_movie.html', {'movies': movies})
+    return render(request, 'html/movie.html', {'movies': movies})
