2232c9aa6bc26f9e81e2c488afff9886056903f7
AllokeyFS <atosh.rami@gmail.com>
2023-07-14 12:52:24 +0600

    added views, urls and html files

---
+++ shop/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import author_list, book_list, category_list, movie_list
+
+
+urlpatterns = [
+    path('author/', author_list, name='author_list'),
+    path('book/', book_list, name='book_list'),
+    path('category/', category_list, name='category_list'),
+    path('movie/', movie_list, name='movie_list'),
+]
--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Author, Book, CategoryMovie, Movie
 
-# Create your views here.
+def author_list(request):
+    authors = Author.objects.all()
+    return render(request, 'html/index.html', {'authors': authors})
+
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'html/book.html', {'books': books})
+
+def category_list(request):
+    categories = CategoryMovie.objects.all()
+    return render(request, 'html/categorymovie.html', {'categories': categories})
+
+def movie_list(request):
+    movies = Movie.objects.all()
+    return render(request, 'html/movie.html', {'movies': movies})
